0d525e7954d79120ef4b50ac282e25988fbc1f12
Hotinec Artiom <hotinec2@gmail.com>
2020-10-05 15:28:33 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let checking = true;
+    while (checking) {
+        checking = false;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let configStr = bracketsConfig[i].join("");
+            if (str.includes(configStr)) {
+                str = str.replace(configStr, "");
+                checking = true;
+            }
+        }
+    }
+
+    return str.length ? false : true;
+};
